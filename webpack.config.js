f48975d9d3a98b167ac92c6f0c1727c7520c759d
Tracy Elmore <tzelmore@gmail.com>
2022-07-18 15:38:07 -0700

    configuring more packages and begin styling

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,30 @@
+const path = require('path');
+
+const postCSSPlugins = [
+    require('postcss-import'),
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
+    require('autoprefixer')
+]
+
 module.exports = {
     mode: 'development',
-    entry: './app/assets/scripts/App.js'
+    entry: './app/assets/scripts/App.js',
+    output: {
+        filename: 'bundled.js',
+        path: path.resolve(__dirname, 'app')
+    },
+    watch: true,
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    'style-loader', 
+                    {loader: "css-loader", options: { url: false}},
+                    {loader: 'postcss-loader', options: {postcssOptions: {plugins: postCSSPlugins}}}
+                ]
+            }
+        ]
+    }
 }
